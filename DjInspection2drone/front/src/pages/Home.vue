c2d41cf214157745d776a9cf7d3297d4ab824962
Sebastián Huérfano <xsebastianhrodriguezx@gmail.com>
2020-04-17 12:48:52 -0500

    Se sube la primera versión del dashboard y la base del sitio web.

---
+++ DjInspection2drone/front/src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template> 
+    <StarterHeader></StarterHeader>   
+</template>
+
+<script>
+import StarterHeader from "@/components/website/StarterHeader";
+export default {
+  components: {
+    StarterHeader
+  }
+};
+</script>
